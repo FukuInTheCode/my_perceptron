5850b4ff137bff85bfc7dc7d451af37276c728cf
FukuInTheCode <tomc.code@gmail.com>
2023-08-09 00:08:19 +0200

    free is now a variadic functionbs

--- src/func/perceptronFree.c
+++ src/func/perceptronFree.c
@@ -1,5 +1,15 @@
 #include "../../includes/my.h"
 
-void my_Perceptron_Free(my_Perceptron *P) {
+void __FreePerceptron(my_Perceptron *P) {
     my_Matrix_Free(1, &(P->theta));
+}
+
+void my_Perceptron_Free(const unsigned int count, ...) {
+    va_list args;
+    va_start(args, count);
+    unsigned int i;
+    for(i=0; i<count; i++) {
+        my_Perceptron *P = va_arg(args, my_Perceptron *);
+        __FreePerceptron(P);
+    }
 }
