f1f15a80c5f096f63e505138ea21f556bb45f47d
FukuInTheCode <tomc.code@gmail.com>
2023-08-08 23:43:26 +0200

    rand bug fix

--- src/libs/matrixRand.c
+++ src/libs/matrixRand.c
@@ -1,25 +1,41 @@
 #include "../../includes/my.h"
 
-void __Rand(my_Matrix *A, int minN, int maxN) {
-    maxN++;
-    if(minN>maxN) {
-        SWAP(minN, maxN)
-    }
+void __RandFloat(my_Matrix *A, double minN, double maxN) {
     unsigned int i, j;
     for(i = 0; i<A->m; i++) {
         for(j = 0; j<A->n; j++) {
-            my_Matrix_Set(A, j, i, rand() % (maxN - minN) + minN);
+            my_Matrix_Set(A, j, i, my_randFloat(minN, maxN));
         }
     }
 }
 
-void my_Matrix_Rand(int minN, int maxN, const unsigned int count, ...) {
+void my_Matrix_RandFloat(double minN, double maxN, const unsigned int count, ...) {
     va_list args;
     va_start(args, count);
     unsigned int i;
     for(i = 0; i<count; i++) {
         my_Matrix *A = va_arg(args, my_Matrix *);
-        __Rand(A, minN, maxN);
+        __RandFloat(A, minN, maxN);
+    }
+    va_end(args);
+}
+
+void __RandInt(my_Matrix *A, int minN, int maxN) {
+    unsigned int i, j;
+    for(i = 0; i<A->m; i++) {
+        for(j = 0; j<A->n; j++) {
+            my_Matrix_Set(A, j, i, my_randInt(minN, maxN));
+        }
+    }
+}
+
+void my_Matrix_RandInt(int minN, int maxN, const unsigned int count, ...) {
+    va_list args;
+    va_start(args, count);
+    unsigned int i;
+    for(i = 0; i<count; i++) {
+        my_Matrix *A = va_arg(args, my_Matrix *);
+        __RandInt(A, minN, maxN);
     }
     va_end(args);
 }
--- src/main.c
+++ src/main.c
@@ -1,6 +1,17 @@
 #include "../includes/my.h"
 
 int main(int argc, char* argv[]) {
+    my_Matrix A = {.m=0, .n=0};
+    my_Matrix_Create(2, 2, 1, &A);
+    my_Matrix_RandFloat(-10, 10, 1, &A);
     
+    my_Matrix B = {.m=0, .n=0};
+    my_Matrix_Create(2, 2, 1, &B);
+    my_Matrix_RandInt(-10, 10, 1, &B);
+
+    my_Matrix_Print(2, &A, &B);
+
+    my_Matrix_Free(2, &A, &B);
+
     return 0;
 }
