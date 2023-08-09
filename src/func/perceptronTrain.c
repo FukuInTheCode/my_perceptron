906f9e35286591ac92ff30d80163471e4c01a191
FukuInTheCode <tomc.code@gmail.com>
2023-08-09 23:06:04 +0200

    perceptron error is added

---
+++ src/func/perceptronTrain.c
@@ -0,0 +1,8 @@
+#include "../../includes/my.h"
+
+void my_Perceptron_Train(my_Perceptron *P, my_Matrix *inputs, my_Matrix *targets, const unsigned double alpha, const unsigned int stepsN) {
+    unsigned int i;
+    for(i=0; i<stepsN; i++) {
+        
+    }
+}
