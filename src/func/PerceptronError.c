497efcea9162fe6817c4e2fc35aa23d9d3e85392
FukuInTheCode <tomc.code@gmail.com>
2023-08-09 20:08:26 +0200

    start of calcError

--- src/func/PerceptronError.c
+++ src/func/PerceptronError.c
@@ -1,3 +1,9 @@
 #include "../../includes/my.h"
 
-double my_Perceptron_calcError(my_Perceptron *entity, my_Matrix *inputs, my_Matrix *targets);
+double my_Perceptron_calcError(my_Perceptron *P, my_Matrix *inputs, my_Matrix *targets) {
+    my_Matrix *Prediction = {.m=0, .n=0};
+
+    my_Perceptron_Predict(P, inputs, &Prediction);
+
+    my_Matrix_Free(1, &Prediction);
+}
