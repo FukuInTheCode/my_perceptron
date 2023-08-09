c38dba14bf100494bd39a61527a28aefd174c59e
FukuInTheCode <tomc.code@gmail.com>
2023-08-09 20:05:47 +0200

    predit functions

--- includes/my_perceptron.h
+++ includes/my_perceptron.h
@@ -9,3 +9,4 @@ typedef struct my_Perceptron {
 void my_Perceptron_Create(const unsigned int inputsSize, const unsigned int count, ...);
 void my_Perceptron_Free(const unsigned int count, ...);
 void my_Perceptron_Print(const unsigned int count, ...);
+void my_Perceptron_Predict(my_Perceptron *P, my_Matrix *inputs, my_Matrix *predictions);
