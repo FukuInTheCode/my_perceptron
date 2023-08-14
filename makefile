530e57a21a231eee8fd925cd5a58da523ce7a202
FukuInTheCode <tomc.code@gmail.com>
2023-08-14 17:38:18 +0200

    change in the makefile now support libs

--- makefile
+++ makefile
@@ -2,19 +2,25 @@ NAME = my_Perceptron
 
 CFLAGS = -W -Wall -Wextra
 
-SRC = $(wildcard ./src/*.c) \
-		$(wildcard ./src/func/*.c) \
-			$(wildcard ./src/libs/*.c)
+LIBS = -l$(NAME) -lmy_math -lmy_matrix
+
+SRC = $(wildcard ./src/*.c)
+
+LIB_SRC = $(wildcard ./src/func/*.c)
+
+LIB_OBJ = $(LIB_SRC:.c=.o)
 
 OBJ = $(SRC:.c=.o)
 
-all: build clean
+all: archive build clean
+
+archive: $(LIB_OBJ)
+	ar -rcs "C:\Users\ADMIN\Code\my_libs\lib"$(NAME).a $(LIB_OBJ)
 
 build: $(OBJ)
 	@if not exist "./out" mkdir "./out"
-	@gcc $(CFLAGS) $(OBJ) -o ./out/$(NAME)
+	@gcc $(CFLAGS) $(OBJ) -L"C:\Users\ADMIN\Code\my_libs" $(LIBS) -o ./out/$(NAME)
 
 clean:
 	@if exist ./src/*.o del /Q src\*.o
 	@if exist ./src/func/*.o del /Q src\func\*.o
-	@if exist ./src/libs/*.o del /Q src\libs\*.o
--- src/main.c
+++ src/main.c
@@ -7,9 +7,38 @@ double __mysq(double x) {
 int main(int argc, char* argv[]) {
     srand(time(NULL));
 
+    my_Matrix features = {.m = 0, .n =0};
+    my_Matrix targets = {.m = 0, .n =0};
+    my_Matrix prediction = {.m = 0, .n =0};
+
+    my_Matrix_Create(2, 10, 1, &features);
+    my_Matrix_Create(2, 1, 1, &targets);
+    my_Matrix_RandInt(0, 20, 2, &features, &targets);
+
+    my_Matrix_Print(2, &features, &targets);
+
     my_Perceptron P = {.theta.m = 0, .theta.n = 0};
 
+    my_Perceptron_Create(10, 1, &P);
+    my_Perceptron_Print(1, &P);
+
+    printf("%f\n", my_Perceptron_calcErrorMSE(&P, &features, &targets));
+    my_Perceptron_Predict(&P, &features, &prediction);
+    my_Matrix_Print(1, &prediction);
+
+    my_Perceptron_Train(&P, &features, &targets, 0.001, 10000);
+    
+    printf("Training completed!\n");
+
+    printf("%f\n", my_Perceptron_calcErrorMSE(&P, &features, &targets));
+    my_Perceptron_Predict(&P, &features, &prediction);
+    my_Matrix_Print(1, &prediction);
+
+    my_Perceptron_Print(1, &P);
+
     my_Perceptron_Free(1, &P);
+
+    my_Matrix_Free(3, &features, &targets, &prediction);
 
     return 0;
 }
